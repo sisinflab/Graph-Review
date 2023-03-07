2221bacbf6f220dbc8d9cfb197edf8c3a37c1319
dmalitesta <d.malitesta@gmail.com>
2023-03-07 16:59:43 +0100

    Added config file with varying seeds.

--- external/models/rmg/RMGAll.py
+++ external/models/rmg/RMGAll.py
@@ -12,8 +12,10 @@ nltk.download('punkt')
 
 parser = argparse.ArgumentParser(description="Run preprocessing for RMG.")
 parser.add_argument('--dataset', nargs='?', default='Digital_Music', help='dataset path')
+parser.add_argument('--batch_size', default=128, help='batch size')
 args = parser.parse_args()
 dataset = args.dataset
+batch_size = args.batch_size
 
 with open(f'../../../data/{dataset}/{dataset}_5.json', 'r') as f:
     rawdata = [JSONDecoder().decode(x) for x in f.readlines()]
@@ -315,118 +317,114 @@ def generate_batch_data_random(item,user,user_to_item_to_user,ui,item_to_user_to
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-from tensorflow import keras
-from keras.models import Model
-from keras.layers import *
-from keras.optimizers import Adam
+import tensorflow as tf
+
+sentence_input = tf.keras.layers.Input(shape=(MAX_SENT_LENGTH,), dtype='int32')
+embedding_layer = tf.keras.layers.Embedding(len(word_dict_freq), 300, weights=[emb_mat],trainable=True)
+
+embedded_sequences = tf.keras.layers.Dropout(0.2)(embedding_layer(sentence_input))
+
+word_cnn_fea = tf.keras.layers.Dropout(0.2)(tf.keras.layers.Convolution1D(nb_filter=100, filter_length=3,  padding='same', activation='relu', strides=1)(embedded_sequences))
+
+word_att = tf.keras.layers.Dense(100,activation='tanh')(word_cnn_fea)
+word_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(word_att))
+word_att = tf.keras.layers.Activation('softmax')(word_att)
+sent_emb=tf.keras.layers.Dot((1, 1))([word_cnn_fea, word_att])
+
+sent_encoder = tf.keras.Model([sentence_input], sent_emb)
+
+review_input = tf.keras.Input((MAX_SENTS,MAX_SENT_LENGTH,), dtype='int32')
+
+review_encoder = tf.keras.layers.TimeDistributed(sent_encoder)(review_input)
+
+sent_cnn_fea = tf.keras.layers.Dropout(0.2)(tf.keras.layers.Convolution1D(nb_filter=100, filter_length=3, padding='same', activation='relu', strides=1)(review_encoder))
+
+sent_att = tf.keras.layers.Dense(100,activation='tanh')(sent_cnn_fea)
+sent_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(sent_att))
+word_att = tf.keras.layers.Activation('softmax')(sent_att)
+doc_emb=tf.keras.layers.Dot((1, 1))([sent_cnn_fea, word_att])
+
+doc_encoder = tf.keras.Model([review_input], doc_emb)
 
 
-sentence_input = Input(shape=(MAX_SENT_LENGTH,), dtype='int32')
-embedding_layer = Embedding(len(word_dict_freq), 300, weights=[emb_mat],trainable=True)
+reviews_input_item = tf.keras.Input((MAX_REVIEW_ITEM,MAX_SENTS,MAX_SENT_LENGTH,), dtype='int32')
+reviews_input_user = tf.keras.Input((MAX_REVIEW_USER,MAX_SENTS,MAX_SENT_LENGTH,), dtype='int32')
 
-embedded_sequences = Dropout(0.2)(embedding_layer(sentence_input))
-
-word_cnn_fea = Dropout(0.2)(Convolution1D(nb_filter=100, filter_length=3,  padding='same', activation='relu', strides=1)(embedded_sequences))
-
-word_att = Dense(100,activation='tanh')(word_cnn_fea)
-word_att = Flatten()(Dense(1)(word_att))
-word_att = Activation('softmax')(word_att)
-sent_emb=Dot((1, 1))([word_cnn_fea, word_att])
-
-sent_encoder = Model([sentence_input], sent_emb)
-
-review_input = keras.Input((MAX_SENTS,MAX_SENT_LENGTH,), dtype='int32')
-
-review_encoder = TimeDistributed(sent_encoder)(review_input)
-
-sent_cnn_fea = Dropout(0.2)(Convolution1D(nb_filter=100, filter_length=3, padding='same', activation='relu', strides=1)(review_encoder))
-
-sent_att = Dense(100,activation='tanh')(sent_cnn_fea)
-sent_att = Flatten()(Dense(1)(sent_att))
-word_att = Activation('softmax')(sent_att)
-doc_emb=keras.layers.Dot((1, 1))([sent_cnn_fea, word_att])
-
-doc_encoder = Model([review_input], doc_emb)
+reviews_emb_item = tf.keras.layers.TimeDistributed(doc_encoder)(reviews_input_item)
+reviews_emb_user = tf.keras.layers.TimeDistributed(doc_encoder)(reviews_input_user)
 
 
-reviews_input_item = keras.Input((MAX_REVIEW_ITEM,MAX_SENTS,MAX_SENT_LENGTH,), dtype='int32')
-reviews_input_user = keras.Input((MAX_REVIEW_USER,MAX_SENTS,MAX_SENT_LENGTH,), dtype='int32')
+doc_att = tf.keras.layers.Dense(100,activation='tanh')(reviews_emb_item)
+doc_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(doc_att))
+doc_att = tf.keras.layers.Activation('softmax')(doc_att)
+item_emb=tf.keras.layers.Dot((1, 1))([reviews_emb_item, doc_att])
 
-reviews_emb_item = TimeDistributed(doc_encoder)(reviews_input_item)
-reviews_emb_user = TimeDistributed(doc_encoder)(reviews_input_user)
+doc_att_u = tf.keras.layers.Dense(100,activation='tanh')(reviews_emb_user)
+doc_att_u = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(doc_att_u))
+doc_att_u = tf.keras.layers.Activation('softmax')(doc_att_u)
+user_emb=tf.keras.layers.Dot((1, 1))([reviews_emb_user, doc_att_u])
 
-
-doc_att = Dense(100,activation='tanh')(reviews_emb_item)
-doc_att = Flatten()(Dense(1)(doc_att))
-doc_att = Activation('softmax')(doc_att)
-item_emb=keras.layers.Dot((1, 1))([reviews_emb_item, doc_att])
-
-doc_att_u = Dense(100,activation='tanh')(reviews_emb_user)
-doc_att_u = Flatten()(Dense(1)(doc_att_u))
-doc_att_u = Activation('softmax')(doc_att_u)
-user_emb=keras.layers.Dot((1, 1))([reviews_emb_user, doc_att_u])
-
-user_id = Input(shape=(1,), dtype='int32')
-item_id = Input(shape=(1,), dtype='int32')
+user_id =tf.keras.layers. Input(shape=(1,), dtype='int32')
+item_id = tf.keras.layers.Input(shape=(1,), dtype='int32')
 
 
-user_embedding= Embedding(len(user_review_id),100,trainable=True)
-item_embedding = Embedding(len(item_review_id), 100,trainable=True)
+user_embedding= tf.keras.layers.Embedding(len(user_review_id),100,trainable=True)
+item_embedding = tf.keras.layers.Embedding(len(item_review_id), 100,trainable=True)
 
-user_item_ids = keras.Input((MAX_NEIGHBOR,), dtype='int32')
-item_user_ids = keras.Input((MAX_NEIGHBOR,), dtype='int32')
+user_item_ids = tf.keras.Input((MAX_NEIGHBOR,), dtype='int32')
+item_user_ids = tf.keras.Input((MAX_NEIGHBOR,), dtype='int32')
 
-user_item_user_ids = keras.Input((MAX_NEIGHBOR,MAX_NEIGHBOR), dtype='int32')
-item_user_item_ids = keras.Input((MAX_NEIGHBOR,MAX_NEIGHBOR), dtype='int32')
+user_item_user_ids = tf.keras.Input((MAX_NEIGHBOR,MAX_NEIGHBOR), dtype='int32')
+item_user_item_ids = tf.keras.Input((MAX_NEIGHBOR,MAX_NEIGHBOR), dtype='int32')
 
 user_item_embedding= user_embedding(user_item_ids)
 item_user_embedding= item_embedding(item_user_ids)
 
-ui_att = Dense(100,activation='tanh')(user_item_embedding)
-ui_att = Flatten()(Dense(1)(ui_att))
-ui_att = Activation('softmax')(ui_att)
-ui_emb=keras.layers.Dot((1, 1))([user_item_embedding, ui_att])
+ui_att = tf.keras.layers.Dense(100,activation='tanh')(user_item_embedding)
+ui_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(ui_att))
+ui_att = tf.keras.layers.Activation('softmax')(ui_att)
+ui_emb=tf.keras.layers.Dot((1, 1))([user_item_embedding, ui_att])
 
-iu_att = Dense(100,activation='tanh')(item_user_embedding)
-iu_att = Flatten()(Dense(1)(iu_att))
-iu_att_weight = Activation('softmax')(iu_att)
-iu_emb=keras.layers.Dot((1, 1))([item_user_embedding, iu_att_weight])
+iu_att = tf.keras.layers.Dense(100,activation='tanh')(item_user_embedding)
+iu_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(iu_att))
+iu_att_weight = tf.keras.layers.Activation('softmax')(iu_att)
+iu_emb=tf.keras.layers.Dot((1, 1))([item_user_embedding, iu_att_weight])
 
-userencoder = Model([user_item_ids], ui_emb)
-itemencoder = Model([item_user_ids], iu_emb)
+userencoder = tf.keras.Model([user_item_ids], ui_emb)
+itemencoder = tf.keras.Model([item_user_ids], iu_emb)
 
-user_encoder = TimeDistributed(userencoder)(user_item_user_ids)
-item_encoder = TimeDistributed(itemencoder)(item_user_item_ids)
+user_encoder = tf.keras.layers.TimeDistributed(userencoder)(user_item_user_ids)
+item_encoder = tf.keras.layers.TimeDistributed(itemencoder)(item_user_item_ids)
 
-ufactor=concatenate([user_item_embedding,user_encoder])
-ifactor=concatenate([item_user_embedding,item_encoder])
+ufactor=tf.keras.layers.concatenate([user_item_embedding,user_encoder])
+ifactor=tf.keras.layers.concatenate([item_user_embedding,item_encoder])
 
-un_att = Dense(100,activation='tanh')(ufactor)
-un_att = Flatten()(Dense(1)(un_att))
-un_att = Activation('softmax')(un_att)
-user_emb_g=keras.layers.Dot((1, 1))([ufactor, un_att])
+un_att = tf.keras.layers.Dense(100,activation='tanh')(ufactor)
+un_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(un_att))
+un_att = tf.keras.layers.Activation('softmax')(un_att)
+user_emb_g=tf.keras.layers.Dot((1, 1))([ufactor, un_att])
 
-in_att = Dense(100,activation='tanh')(ifactor)
-in_att = Flatten()(Dense(1)(in_att))
-in_att = Activation('softmax')(in_att)
-item_emb_g=keras.layers.Dot((1, 1))([ifactor, in_att])
+in_att = tf.keras.layers.Dense(100,activation='tanh')(ifactor)
+in_att = tf.keras.layers.Flatten()(tf.keras.layers.Dense(1)(in_att))
+in_att = tf.keras.layers.Activation('softmax')(in_att)
+item_emb_g=tf.keras.layers.Dot((1, 1))([ifactor, in_att])
 
 
-user_embedding= Flatten()(user_embedding(user_id))
-item_embedding= Flatten()(item_embedding(item_id))
-factor_u=concatenate([user_emb,user_embedding,user_emb_g])
-factor_i=concatenate([item_emb,item_embedding,item_emb_g])
+user_embedding= tf.keras.layers.Flatten()(user_embedding(user_id))
+item_embedding= tf.keras.layers.Flatten()(item_embedding(item_id))
+factor_u=tf.keras.layers.concatenate([user_emb,user_embedding,user_emb_g])
+factor_i=tf.keras.layers.concatenate([item_emb,item_embedding,item_emb_g])
 
-preds=Dense(1,activation='relu')(multiply([factor_u,factor_i]))
+preds=tf.keras.layers.Dense(1,activation='relu')(tf.multiply([factor_u,factor_i]))
 
-model = Model([reviews_input_item,reviews_input_user,user_item_user_ids,user_item_ids,item_user_item_ids,item_user_ids,item_id,user_id], preds)
+model = tf.keras.Model([reviews_input_item,reviews_input_user,user_item_user_ids,user_item_ids,item_user_item_ids,item_user_ids,item_id,user_id], preds)
 
-model.compile(loss='mse', optimizer=Adam(lr=0.001), metrics=['mse'])
+model.compile(loss='mse', optimizer=tf.optimizers.Adam(lr=0.001), metrics=['mse'])
 
 
 for ep in range(1):
-    traingen=generate_batch_data_random(all_item_texts,all_user_texts,user_to_item_to_user,user_to_item,item_to_user_to_item,item_to_user,train_item_id,train_user_id,train_label,24)
-    valgen=generate_batch_data_random(all_item_texts,all_user_texts,user_to_item_to_user,user_to_item,item_to_user_to_item,item_to_user,test_item_id,test_user_id,test_label,24)
-    model.fit_generator(traingen, epochs=1,steps_per_epoch=len(train_item_id)//24)
-    cr = model.evaluate_generator(valgen, steps=len(test_item_id)//24)
+    traingen=generate_batch_data_random(all_item_texts,all_user_texts,user_to_item_to_user,user_to_item,item_to_user_to_item,item_to_user,train_item_id,train_user_id,train_label,batch_size)
+    valgen=generate_batch_data_random(all_item_texts,all_user_texts,user_to_item_to_user,user_to_item,item_to_user_to_item,item_to_user,test_item_id,test_user_id,test_label,512)
+    model.fit_generator(traingen, epochs=1,steps_per_epoch=len(train_item_id)//batch_size)
+    cr = model.evaluate_generator(valgen, steps=len(test_item_id)//512)
     print(np.sqrt(cr))
